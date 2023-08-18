0b12241a5fbf24d6e9f45ac3f6e5f49cab1efa24
kingjamesx <talk2james.uj@gmail.com>
2023-08-18 08:11:47 +0100

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import { useEffect } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import MovieDetails from './pages/MovieDetails'
 import Error from './pages/Error'
 import { Routes,Route } from 'react-router-dom'
 function App() {
-  useEffect(() => {
-    const handleMessage = (event) => {
-      if (event.data && event.data.type === 'requestClickInfo') {
-        const clickInfo = {
-          urlChanges: window.location.href,
-        };
-
-        window.parent.postMessage({ type: 'iframeClick', clickInfo }, '*');
-      }
-    };
-
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  }, []);
-
   return (
     <div className="App">
       <Routes>
