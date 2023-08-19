e2f860283796dc3ae89ad98bf8928d8e8f76f7de
kingjamesx <talk2james.uj@gmail.com>
2023-08-19 18:10:27 +0100

    add script

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   // Inside the iframe
       useEffect(
         ()=>{
-          !function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.$=o():e.$=o()}(self,(()=>(window.onload=function(){var e=document.location.href,o=document.querySelector("body");new MutationObserver((function(o){if(e!==document.location.href){e=document.location.href;var t=JSON.parse(JSON.stringify(e));parent.postMessage(t,"https://staging.crowdapp.io")}})).observe(o,{childList:!0,subtree:!0})},{})));
+          !function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.$=o():e.$=o()}(self,(()=>(window.onload=function(){var e=document.location.href,o=document.querySelector("body");new MutationObserver((function(o){if(e!==document.location.href){e=document.location.href;var t=JSON.parse(JSON.stringify(e));parent.postMessage(t,"http://localhost:2222/")}})).observe(o,{childList:!0,subtree:!0})},{})));
         },[]
       )
   return (
